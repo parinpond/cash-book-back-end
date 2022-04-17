c73b8628d3b5abbf625ef5b137c11b5ad8f6fc83
Parinyimz <40853593+parinpond@users.noreply.github.com>
2022-04-17 16:01:51 +0700

    update route

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,9 @@
-require("dotenv").config();
 const express = require('express')
 const app = express()
-const PORT = process.env.NODE_DOCKER_PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+app.listen(3002, () => {
+  console.log('Start server at port 3002.')
+})
